b3910de37e866dd2d92491a36f4d44757afda8e1
revgato <locchoihay147@gmail.com>
2022-04-11 11:07:54 +0700

    Update week2

---
+++ week2/test/mytest.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# ./scanner ../test/example1.kpl | diff ../test/result1.txt -
+./scanner ../test/example2.kpl | diff ../test/result2.txt -
+./scanner ../test/example3.kpl | diff ../test/result3.txt -
+./scanner ../test/example4.kpl | diff ../test/result4.txt -
+# ./scanner ../test/example5.kpl | diff ../test/result5.txt -
+./scanner ../test/example6.kpl | diff ../test/result6.txt -
+# Example 1 và example5 đang bị lỗi phần result, không nên test 2 file này
